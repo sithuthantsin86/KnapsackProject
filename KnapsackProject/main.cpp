b15f39b14975b377df1f56dcb4364af799dd24ed
sithuthantsin86 <sithuthantsin86@gmail.com>
2016-01-01 16:07:15 +0300

    Initial Commit

---
+++ KnapsackProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  KnapsackProject
+//
+//  Created by Si Thu Thant Sin on 01/01/16.
+//  Copyright © 2016 Defence Services Science and Technology Research Center. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
